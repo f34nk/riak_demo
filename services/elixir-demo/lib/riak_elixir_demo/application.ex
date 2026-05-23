16a22b8e4ba2c7ad5cc3a1c11edfc65357e4728b
Frank Eickhoff <info@frankeickhoff.de>
2026-05-23 22:16:57 +0200

    feat: add Elixir demo application entrypoint

---
+++ services/elixir-demo/lib/riak_elixir_demo/application.ex
@@ -0,0 +1,18 @@
+defmodule RiakElixirDemo.Application do
+  @moduledoc false
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    try do
+      RiakElixirDemo.run()
+      System.halt(0)
+    rescue
+      error ->
+        IO.puts(Exception.format(:error, error, __STACKTRACE__))
+        System.halt(1)
+    end
+
+    {:ok, self()}
+  end
+end
