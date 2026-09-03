5d123e28600c79b50605b223e32908a0cc661b88
Frank Eickhoff <info@frankeickhoff.de>
2026-09-03 09:38:13 +0200

    fix: pass integrations to BeamProtocolCodegenFactory.create

--- services/erlang-demo/codegen/src/main/java/openriak/erlang/codegen/OpenRiakBeamProtocolCodegenFactory.java
+++ services/erlang-demo/codegen/src/main/java/openriak/erlang/codegen/OpenRiakBeamProtocolCodegenFactory.java
@@ -3,17 +3,23 @@ package openriak.erlang.codegen;
 import io.smithy.beam.core.BeamHttpBindings;
 import io.smithy.beam.core.BeamProtocolCodegen;
 import io.smithy.beam.core.BeamProtocolCodegenFactory;
+import io.smithy.beam.core.BeamProtocolIntegration;
 import software.amazon.smithy.model.Model;
 import software.amazon.smithy.model.shapes.ShapeId;
+
+import java.util.List;
 
 public final class OpenRiakBeamProtocolCodegenFactory {
 
     private OpenRiakBeamProtocolCodegenFactory() {}
 
-    public static BeamProtocolCodegen create(Model model, ShapeId resolvedProtocolTraitId) {
+    public static BeamProtocolCodegen create(
+            Model model,
+            ShapeId resolvedProtocolTraitId,
+            List<? extends BeamProtocolIntegration> integrations) {
         if (OpenRiakHttpProtocolCodegen.OPEN_RIAK_HTTP.equals(resolvedProtocolTraitId)) {
             return new OpenRiakHttpProtocolCodegen(BeamHttpBindings.from(model));
         }
-        return BeamProtocolCodegenFactory.create(model, resolvedProtocolTraitId);
+        return BeamProtocolCodegenFactory.create(model, resolvedProtocolTraitId, integrations);
     }
 }
